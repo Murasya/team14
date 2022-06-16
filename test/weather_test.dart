e273d6b8bcdd30bdd916a9feb8d1cd9f62d7b4cf
F <31436@toyota.kosen-ac.jp>
2022-06-17 00:19:16 +0900

    remove API key

--- test/weather_test.dart
+++ test/weather_test.dart
@@ -4,7 +4,7 @@ import 'package:team14/weather.dart';
 void main() async {
   try {
     var jsonResponse = await getWeather(
-      appId: 'dj00aiZpPVhBbmd1Q3hQaDdpYSZzPWNvbnN1bWVyc2VjcmV0Jng9Y2Y-',
+      appId: '<appid>',
       coordinates: '139.752686,35.685148',
       output: 'json',
       interval: 10,
