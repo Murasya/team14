ef50092fa285a8170639e11ba1a9d6568b710ed4
Yuki992411 <k.yuki.9924.11@gmail.com>
2022-06-18 03:33:13 +0900

    tableへのデータ追加・全件取得処理の追加
    * `Spot`クラスに，
      * `Map<String, Object?>`との変換を行なう関数を追加
      * メンバ変数のダンプ用関数を追加

--- lib/spot.dart
+++ lib/spot.dart
@@ -24,4 +24,32 @@ class Spot {
     this.createdAt,
     this.updatedAt,
   );
+
+  Map<String, Object?> toMap() {
+    var map = <String, Object>{
+      columnId: id,
+      columnTitle: title,
+      columnTemperature: temperature,
+      columnGpsPosition: gpsPosition,
+      columnMemo: memo,
+      columnCreatedAt: createdAt,
+      columnUpdatedAt: updatedAt,
+    };
+    return map;
+  }
+
+  Spot.fromMap(Map<String, Object?> map)
+      : id = map[columnId] as int,
+        title = map[columnTitle] as String,
+        temperature = map[columnTemperature] as num,
+        gpsPosition = map[columnGpsPosition] as String,
+        memo = map[columnMemo] as String,
+        createdAt = map[columnCreatedAt] as String,
+        updatedAt = map[columnUpdatedAt] as String;
+
+  // For Debug
+  void dumpAllColumns() {
+    print(
+        '$id: $title, $temperature, $gpsPosition, $memo, $createdAt, $updatedAt');
+  }
 }
---
+++ lib/spotProvider.dart
@@ -0,0 +1,52 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'spot.dart';
+
+const String dbName = 'memo-app.db';
+const String tableName = 'spot';
+
+class SpotProvider {
+  late Database db;
+
+  Future open() async {
+    final dbDirectory = await getApplicationSupportDirectory();
+    final dbFilePath = dbDirectory.path;
+    db = await openDatabase(join(dbFilePath, dbName), version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+create table $tableName (
+  $columnId integer primary key autoincrement,
+  $columnTitle text not null,
+  $columnTemperature real not null,
+  $columnGpsPosition text not null,
+  $columnMemo text not null,
+  $columnCreatedAt text not null,
+  $columnUpdatedAt text not null)
+''');
+    });
+  }
+
+  Future<int> insert(Spot spot) async {
+    return await db.insert(tableName, spot.toMap());
+  }
+
+  Future<List<Spot>> selectAll() async {
+    List<Map<String, Object?>> maps = await db.query(tableName, columns: [
+      columnId,
+      columnTitle,
+      columnTemperature,
+      columnTemperature,
+      columnGpsPosition,
+      columnMemo,
+      columnCreatedAt,
+      columnUpdatedAt,
+    ]);
+    List<Spot> spots = <Spot>[];
+    for (var map in maps) {
+      Spot spot = Spot.fromMap(map);
+      spots.add(spot);
+    }
+    return Future<List<Spot>>.value(spots);
+  }
+}
