43c65b3aaf5b654dbab9e513316518bc7ffd7ef5
Yuki992411 <k.yuki.9924.11@gmail.com>
2022-06-21 08:37:02 +0900

    メモのテンプレ用のDB接続
    * `spotProvider`をベースに作成
    * `selectLatestMemoTemplate()`は，テンプレ作成時に使用

--- lib/memoTemplate.dart
+++ lib/memoTemplate.dart
@@ -1,3 +1,9 @@
+const String memoTemplateColumnId = '_id';
+const String memoTemplateColumnName = 'name';
+const String memoTemplateColumnTextBox = 'text_box';
+const String memoTemplateColumnRadioButtonLists = 'radio_button_lists';
+const String memoTemplateColumnPullDownLists = 'pull_down_lists';
+
 class MemoTemplate {
   int? id;
   String name;
@@ -11,6 +17,25 @@ class MemoTemplate {
     this.radioButtonLists,
     this.pullDownLists,
   );
+
+  Map<String, Object?> toMap() {
+    var map = <String, Object>{
+      memoTemplateColumnName: name,
+      memoTemplateColumnTextBox: textBox == true ? 1 : 0,
+      memoTemplateColumnRadioButtonLists: radioButtonLists.join('\n'),
+      memoTemplateColumnPullDownLists: pullDownLists.join('\n'),
+    };
+    return map;
+  }
+
+  MemoTemplate.fromMap(Map<String, Object?> map)
+      : id = map[memoTemplateColumnId] as int,
+        name = map[memoTemplateColumnName] as String,
+        textBox = map[memoTemplateColumnTextBox] == 1,
+        radioButtonLists =
+            map[memoTemplateColumnRadioButtonLists].toString().split('\n'),
+        pullDownLists =
+            map[memoTemplateColumnPullDownLists].toString().split('\n');
 
   @override
   String toString() =>
---
+++ lib/memoTemplateProvider.dart
@@ -0,0 +1,68 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'memoTemplate.dart';
+
+const String dbName = 'memo-app.db';
+const String memoTemplateTableName = 'memo_template';
+
+class MemoTemplateProvider {
+  Future<Database> _open() async {
+    final dbDirectory = await getApplicationSupportDirectory();
+    final dbFilePath = dbDirectory.path;
+    return await openDatabase(join(dbFilePath, dbName), version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+create table $memoTemplateTableName (
+  $memoTemplateColumnId integer primary key autoincrement,
+  $memoTemplateColumnName text unique not null,
+  $memoTemplateColumnTextBox integer not null,
+  $memoTemplateColumnRadioButtonLists text not null,
+  $memoTemplateColumnPullDownLists text not null)
+''');
+    });
+  }
+
+  Future<int> insert(MemoTemplate memoTemplate) async {
+    final db = await _open();
+    return await db.insert(memoTemplateTableName, memoTemplate.toMap());
+  }
+
+  Future<MemoTemplate?> selectLatestMemoTemplate() async {
+    final db = await _open();
+    final maps = await db.query(
+      memoTemplateTableName,
+      orderBy: '$memoTemplateColumnId DESC',
+      limit: 1,
+    );
+    if (maps.isEmpty) return null;
+    return MemoTemplate.fromMap(maps.first);
+  }
+
+  Future<List<MemoTemplate>> selectAll() async {
+    final db = await _open();
+    final maps = await db.query(
+      memoTemplateTableName,
+      orderBy: '$memoTemplateColumnId DESC',
+    );
+    if (maps.isEmpty) return [];
+    return maps.map((map) => MemoTemplate.fromMap(map)).toList();
+  }
+
+  Future<int> update(MemoTemplate memoTemplate) async {
+    final db = await _open();
+    return await db.update(memoTemplateTableName, memoTemplate.toMap(),
+        where: '$memoTemplateColumnId = ?', whereArgs: [memoTemplate.id]);
+  }
+
+  Future<int> delete(int id) async {
+    final db = await _open();
+    return await db.delete(memoTemplateTableName,
+        where: '$memoTemplateColumnId = ?', whereArgs: [id]);
+  }
+
+  Future close() async {
+    final db = await _open();
+    db.close();
+  }
+}
