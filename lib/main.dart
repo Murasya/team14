bf2db50ee7cd0b3a534dc39f0d66c2b9fd7154d5
F <31436@toyota.kosen-ac.jp>
2022-06-25 23:46:12 +0900

    main.dartの整理

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:team14/views/create_template_page.dart';
-import 'package:team14/views/select_template_page.dart';
-import 'package:team14/views/create_template_page.dart';
 import 'package:team14/views/memo_detail_page.dart';
 
 void main() {
