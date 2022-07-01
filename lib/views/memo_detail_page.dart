5a45985ca351eab63f13653bc9a2682f631a956a
Yuki992411 <k.yuki.9924.11@gmail.com>
2022-07-01 16:01:30 +0900

    メモ詳細画面のファイルのリファクタリング
    *` memo_google_maps_page`と`memo_list_page`を合体

--- lib/views/memo_detail_page.dart
+++ lib/views/memo_detail_page.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
 import 'package:team14/views/common_widgets.dart';
 import 'package:team14/views/detail_helper.dart';
-import 'package:team14/views/memo_google_maps_page.dart';
 import 'package:team14/models/memoTemplate.dart';
 import 'package:team14/models/memo.dart';
 
@@ -22,47 +27,73 @@ class MemoDetailPage extends StatefulWidget {
 class _MemoDetailPageState extends State<MemoDetailPage> {
   @override
   Widget build(BuildContext context) {
+    // map controller
+    Completer<GoogleMapController> controller = Completer();
+
+    // 初期位置
+    final Position initialPosition = Position(
+      latitude: widget.memo.gpsLatitude.toDouble(),
+      longitude: widget.memo.gpsLongitude.toDouble(),
+      timestamp: widget.memo.updatedAt,
+      altitude: 0,
+      accuracy: 0,
+      heading: 0,
+      floor: null,
+      speed: 0,
+      speedAccuracy: 0,
+    );
+
+    // 初期表示座標のMarkerを設定
+    final initialMarkers = {
+      Marker(
+        markerId: MarkerId(initialPosition.timestamp.toString()),
+        position: LatLng(initialPosition.latitude, initialPosition.longitude),
+      ),
+    };
+
     return Scaffold(
       appBar: myAppBar(title: 'メモ詳細', context: context),
-      body: ListView(
-        children: [
-          listItem('タイトル', widget.memo.title),
-          listItem('位置情報',
-              '${widget.memo.gpsLatitude}, ${widget.memo.gpsLongitude}'),
-          // Includes data on precipitation up to one hour ahead.
-          for (var i = 0; i < widget.memo.rainfallList.length; i++)
-            listItem(
-                '降水量(${widget.memo.weatherObsDate.add(Duration(minutes: 60 ~/ (widget.memo.rainfallList.length - 1) * i))})',
-                '${widget.memo.rainfallList.elementAt(i)}'),
-          if (widget.memoTemplate.textBox)
-            listItem('テキスト', '${widget.memo.textBox}'),
-          for (var i = 0;
-              i < widget.memoTemplate.multipleSelectList.length;
-              i++)
-            listItem(widget.memoTemplate.multipleSelectList.elementAt(i),
-                widget.memo.multipleSelectList![i] ? 'はい' : 'いいえ'),
-          if (widget.memoTemplate.singleSelectList.isNotEmpty)
-            listItem(
-                widget.memoTemplate.singleSelectList.first,
-                widget.memoTemplate.singleSelectList
-                    .elementAt(widget.memo.singleSelect!)),
-        ],
+      body: Container(
+        padding: myPadding(),
+        child: Column(children: [
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              myLocationButtonEnabled: false,
+              initialCameraPosition: CameraPosition(
+                target:
+                    LatLng(initialPosition.latitude, initialPosition.longitude),
+                zoom: 14.4746,
+              ),
+              markers: initialMarkers,
+              onMapCreated: controller.complete,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                listItem('タイトル', widget.memo.title),
+                listItem('位置情報',
+                    '${widget.memo.gpsLatitude}, ${widget.memo.gpsLongitude}'),
+                listItem('降水量', '${widget.memo.rainfallList.first}'),
+                if (widget.memoTemplate.textBox)
+                  listItem('テキスト', '${widget.memo.textBox}'),
+                for (var i = 0;
+                    i < widget.memoTemplate.multipleSelectList.length;
+                    i++)
+                  listItem(widget.memoTemplate.multipleSelectList.elementAt(i),
+                      widget.memo.multipleSelectList![i] ? 'はい' : 'いいえ'),
+                if (widget.memoTemplate.singleSelectList.isNotEmpty)
+                  listItem(
+                      widget.memoTemplate.singleSelectList.first,
+                      widget.memoTemplate.singleSelectList
+                          .elementAt(widget.memo.singleSelect!)),
+              ],
+            ),
+          ),
+        ]),
       ),
       drawer: myDrawer(context),
-      floatingActionButton: IconButton(
-        icon: const Icon(Icons.map),
-        onPressed: () {
-          Future(() {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return MemoGoogleMapsPage(memoTemplate: widget.memoTemplate, memo: widget.memo);
-                },
-              ),
-            );
-          });
-        },
-      ),
     );
   }
 }
